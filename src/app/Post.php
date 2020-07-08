a7c15cb110a6c5fc1fb799b944308ee5c21e4598
shuntaro <shun.sea.f@gmail.com>
2020-07-08 16:37:18 +0900

    set relation

--- src/app/Post.php
+++ src/app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'content', 'image',
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
