05b31e6ff84e93691ef9ff8e232dc66649fce116
shuntaro <shun.sea.f@gmail.com>
2020-07-13 00:49:06 +0900

    update seed files

--- src/database/seeds/FoodIngredientsTableSeeder.php
+++ src/database/seeds/FoodIngredientsTableSeeder.php
@@ -11,7 +11,7 @@ class FoodIngredientsTableSeeder extends Seeder
      */
     public function run()
     {
-        $file = new SplFileObject('database/csv/FoodIngredients.csv');
+        $file = new SplFileObject('database/seeds/csvs/FoodIngredients.csv');
         $file->setFlags(
             \SplFileObject::READ_CSV |
             \SplFileObject::READ_AHEAD |
@@ -21,35 +21,35 @@ class FoodIngredientsTableSeeder extends Seeder
 
         $list = [];
 
-        $now = Carbon::now();
+        $now = new DateTime();
         
         foreach($file as $line) {
             $list[] = [
                 "food_name" => $line[0],
                 "energy_kcal" => $line[1],
-                "protein" => $line[3],
-                "fat" => $line[4],
-                "carbon" => $line[5],
-                "dietary_fiber" => $line[6],
-                "natrium" => $line[7],
-                "kalium" => $line[8],
-                "calcium" => $line[9],
-                "magnesium" => $line[10],
-                "iron" => $line[11],
-                "zinc" => $line[12],
-                "vitamin_k" => $line[13],
-                "vitamin_b1" => $line[14],
-                "vitamin_b2" => $line[15],
-                "vitamin_b6" => $line[16],
-                "vitamin_b12" => $line[17],
-                "folic_acid" => $line[18],
-                "vitamin_c" => $line[19],
-                "salt" => $line[20],
+                "protein" => $line[2],
+                "fat" => $line[3],
+                "carbon" => $line[4],
+                "dietary_fiber" => $line[5],
+                "natrium" => $line[6],
+                "kalium" => $line[7],
+                "calcium" => $line[8],
+                "magnesium" => $line[9],
+                "iron" => $line[10],
+                "zinc" => $line[11],
+                "vitamin_k" => $line[12],
+                "vitamin_b1" => $line[13],
+                "vitamin_b2" => $line[14],
+                "vitamin_b6" => $line[15],
+                "vitamin_b12" => $line[16],
+                "folic_acid" => $line[17],
+                "vitamin_c" => $line[18],
+                "salt" => $line[19],
                 "created_at" => $now,
                 "updated_at" => $now,
             ];
         }
 
-        DB::table("FoodIngredients")->insert($list);
+        DB::table("food_ingredients")->insert($list);
     }
 }
